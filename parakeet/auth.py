993c4a75428c325790dd563a1820966930a5a703
Suellen Zeferino Soares <suellenz@ciandt.com>
2018-07-17 10:41:39 -0300

    print variable home_title

--- parakeet/auth.py
+++ parakeet/auth.py
@@ -69,7 +69,9 @@ class LoginPage:
         return self
 
     def redirect_to_home(self):
-        LOG.debug('SUELLEN WAS HERE!')
+        LOG.debug('SUELLEN WAS HERE!==========================')
+        print(self.home_title)
+        LOG.debug('SUELLEN WAS HERE!==========================')
         LOG.debug('redirect_to_home')
         WebDriverWait(self.browser.selenium, self.browser.waiting_time, self.browser.poll_frequency)\
             .until(ec.title_contains(self.home_title))
